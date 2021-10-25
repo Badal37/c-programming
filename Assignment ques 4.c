11272f7999701a74d977e31e31f9ffa7030b8dab
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-10-25 18:52:31 +0530

    Add files via upload

---
+++ Assignment ques 4.c
@@ -0,0 +1,21 @@
+    /*Multiply two floating numbers*/
+    
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float a,b;
+    scanf("%f%f",&a,&b);
+    printf("%f",a*b);
+
+    return 0;
+}
+    
+    
+    
+    
+
+   /* submitted by Badal
+    class=B.tech CSE 1st year
+    section=f
+    roll n=63*/
