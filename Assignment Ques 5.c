fbb51f934107f98b4fdcbdffd7df34cc2800894b
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-10-25 20:51:52 +0530

    Add files via upload

---
+++ Assignment Ques 5.c
@@ -0,0 +1,20 @@
+    /*To find ASCII value of a character*/
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("Enter a character");
+    scanf("%c",&c);
+    
+    printf("The ASCII valueof %c is %d\n",c,c);
+
+    return 0;
+}
+  
+ 
+
+  
+  /*submitted by Badal
+  B.tech 1st year
+  section (f) roll no (63)*/
