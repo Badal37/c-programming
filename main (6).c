6291818a96459a62a7bf2301db1ea40b07b4c958
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-11-11 23:09:01 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,24 @@
+          /*To find volume of sphere*/
+#include <stdio.h>
+
+int main()
+
+{
+    
+  float r, volume;
+  
+  printf("Enter radius");
+  
+  scanf("%f",&r);
+  
+  volume=1.33*3.14*r*r*r;
+  
+  printf("%f",volume);
+
+  return 0;
+  
+}
+
+
+/*submited by Badal
+  sec F & rollno 63*/
