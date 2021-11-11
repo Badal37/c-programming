f45ea6a482e4b22b8d8bb4c65ceca7f715ac4d3b
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-11-11 22:34:59 +0530

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,26 @@
+       /*To find the area of square*/
+#include <stdio.h>
+
+int main()
+{
+    
+  float r, area;
+  
+  printf("Enter radius");
+  
+  scanf("%f",&r);
+  
+  area=3.14*r*r;
+  
+  printf("%f",area);
+  
+  return 0;
+
+    
+}
+
+
+
+ /*submitted by Badal
+ class = Btech.CS
+ sec & rolln = F & 63*/
