d180fda20e4312e24c72d1e89bb05ea2cb1d51d2
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-11-12 22:57:56 +0530

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,23 @@
+      /*To print the integer (entered by the user)*/
+#include <stdio.h>
+
+int main()
+{
+  int num;
+  
+  printf("Enter the num");
+ 
+  scanf("%d",&num);
+  
+  printf("%d",num);
+  
+
+    return 0;
+    
+}
+
+
+  /*submitted by badal
+   sec f
+   roll no 63*/
+ 
