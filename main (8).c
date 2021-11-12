c295c6d3066e03518a3668661f30427000c5542f
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-11-12 23:11:05 +0530

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,24 @@
+          /*To add two integer*/
+#include<stdio.h>
+
+int main()
+
+{
+  int num1,num2,x;
+  
+  printf("Enter the num1 && num2");
+     
+  scanf("%d%d",&num1,&num2);
+  
+  x=num1+num2;
+  
+  printf("sum is %d",x);
+  
+  return 0;
+  
+}
+
+
+   /*submitted by badal
+   sec f
+   roll no 63*/
