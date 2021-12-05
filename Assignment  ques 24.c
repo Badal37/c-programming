7eb34afd0f901501bf1038587e280bdb0d483e5c
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-12-05 21:47:37 +0530

    Add files via upload

---
+++ Assignment  ques 24.c
@@ -0,0 +1,23 @@
+          /*WAP to print power of a number*/
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+  int base,exp,result;
+  
+  printf("Enter a base number and exp number");
+  
+  scanf("%d%d",&base,&exp);
+  
+  result=pow(base,exp);
+  
+  printf("%d",result);
+  
+    return 0;
+}
+
+
+    /* submitted by Badal
+       class&sec= Btech CSE(F)
+       roll no.= 63*/
