089f50a855c74c45c9fd66cf0cc9baea52c96889
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-12-09 08:34:07 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+   
+    printf("Enter your number");
+    
+    scanf("%d%d",&a,&b);
+    
+    printf("Addition is %d\n",a+b);
+    
+    printf("Subtraction is %d\n",a-b);
+    
+    printf("Multiplication is %d\n",a*b);
+    
+    printf("Quotient is %d\n",a/b);
+    
+    printf("Remainder is %d",a%b);
+    
+    return 0;
+}
