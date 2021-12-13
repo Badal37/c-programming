253539d4a3cc920b528eb1d1159af69a335ab5c1
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-12-13 05:38:54 -0800

    Add files via upload

---
+++ main (12).c
@@ -0,0 +1,22 @@
+             /* WAP to print size of int ,float,double,char */
+#include <stdio.h>
+
+int main()
+{
+  int intType;
+  float floatType;
+  double doubleType;
+  char charType;
+  
+    printf("size of int:%lu bytes\n",sizeof (intType) );
+    printf("size of float:%lu bytes\n",sizeof (floatType) );
+    printf("size of double:%lu bytes\n",sizeof (doubleType) );
+    printf("size of char:%lu bytes\n",sizeof (charType));
+    
+    return 0;
+}
+ 
+ 
+ 
+   // submitted by Badal
+   //sec=f(63)
