a10bb5ce7384180acde4f4824955faa444d8c180
Badal37 <92460919+Badal37@users.noreply.github.com>
2021-12-14 09:21:44 -0800

    Add files via upload

---
+++ main (13).c
@@ -0,0 +1,21 @@
+          /* WAP to print square root of a number*/
+#include <stdio.h>
+#include<math.h>
+ 
+int main()
+{
+    int num;
+    
+    printf("Enter the number ");
+    
+    scanf("%d",&num);
+    
+    printf("square root= %f",sqrt(num));
+
+    return 0;
+}
+
+
+
+     //  Submitted by Badal
+     //  Sec=f(63)
